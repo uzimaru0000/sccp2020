7444defd0686037ea45625b905d44c3ba47d8659
uzimaru0000 <shuji365630@gmail.com>
2020-07-30 16:41:50 +0900

    GETとPOSTだけ

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
-	"strconv"
 )
 
 var todoTable map[int]string
@@ -18,11 +17,11 @@ func init() {
 func main() {
 	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/todo", todoHandler)
-	http.HandleFunc("/todo/", todoHandlerWithID)
 
 	fmt.Printf("Server is running...!\n")
-	err := http.ListenAndServe(":8080", nil)
-	fmt.Println(err)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,39 +42,4 @@ func todoHandler(w http.ResponseWriter, r *http.Request) {
 		id++
 		fmt.Fprint(w, "Success")
 	}
-}
-
-func todoHandlerWithID(w http.ResponseWriter, r *http.Request) {
-	query := pathSlice("/todo/", r.URL.Path)
-
-	id, err := strconv.Atoi(query)
-	if err != nil {
-		fmt.Fprintln(w, "The ID has to be a number.")
-		return
-	}
-
-	todo, ok := todoTable[id]
-	if !ok {
-		fmt.Fprintln(w, "Please specify your registered ID")
-		return
-	}
-
-	switch r.Method {
-	case "GET":
-		fmt.Fprintf(w, "%d\t%s\n", id, todo)
-	case "PUT":
-		bufbody := new(bytes.Buffer)
-		bufbody.ReadFrom(r.Body)
-		body := bufbody.String()
-		todoTable[id] = body
-		fmt.Fprintln(w, "success")
-	case "DELETE":
-		delete(todoTable, id)
-		fmt.Fprintln(w, "success")
-	}
-}
-
-func pathSlice(pattern string, path string) string {
-	s := path[len(pattern):]
-	return s
 }
